6425043fa16b776e55815fc21c0bff249a6b8a59
Simon Csaba <mail@scderox.de>
2021-09-07 23:12:01 +0200

    feat: support for new api methods

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports.getUser = async function (id) {
     }));
 };
 
-module.exports.getAutoSyncMembers = async function() {
+module.exports.getAutoSyncMembers = async function () {
     return new Promise((async (resolve, reject) => {
         const res = await centra(`https://sc-network.net/api/users/autoSync`, 'GET').send();
         if (res.statusCode !== 200) {
@@ -18,9 +18,9 @@ module.exports.getAutoSyncMembers = async function() {
         }
         return resolve(JSON.parse(res.body.toString()));
     }));
-}
+};
 
-module.exports.getBetterGlobalGuild = async function(id) {
+module.exports.getBetterGlobalGuild = async function (id) {
     return new Promise((async (resolve, reject) => {
         const res = await centra(`https://api.betterglobal.xyz/guild/${id}/stats`, 'GET').send();
         if (res.statusCode !== 200) {
@@ -28,9 +28,9 @@ module.exports.getBetterGlobalGuild = async function(id) {
         }
         return resolve(JSON.parse(res.body.toString()));
     }));
-}
+};
 
-module.exports.getBetterGlobalUser = async function(id) {
+module.exports.getBetterGlobalUser = async function (id) {
     return new Promise((async (resolve, reject) => {
         const res = await centra(`https://api.betterglobal.xyz/user/${id}/stats`, 'GET').send();
         if (res.statusCode !== 200) {
@@ -38,4 +38,23 @@ module.exports.getBetterGlobalUser = async function(id) {
         }
         return resolve(JSON.parse(res.body.toString()));
     }));
-}
+};
+
+module.exports.getShortLink = async function (slugOrId) {
+    return new Promise((async (resolve, reject) => {
+        const res = await centra(`https://sc-network.net/api/link/${slugOrId}`, 'GET').send();
+        console.log(res.body.toString())
+        if (res.statusCode !== 200) return reject(res.statusCode);
+        return resolve(JSON.parse(res.body.toString()));
+    }));
+};
+
+module.exports.postNewShortLink = async function (authorization, body) {
+    return new Promise((async (resolve, reject) => {
+        const res = await centra('https://sc-network.net/api/link', 'POST').header('Content-Type', 'application/json').header('Authorization', 'Bearer ' + authorization).body(body, 'json').send();
+        if (res.statusCode !== 201) {
+            return reject(res.statusCode);
+        }
+        return resolve();
+    }));
+};
--- test.js
+++ test.js
@@ -5,6 +5,8 @@ async function test() {
     console.log(await apiModule.getUser('413429490342035466'))
     console.log(await apiModule.getBetterGlobalGuild('489786377261678592'))
     console.log(await apiModule.getBetterGlobalUser('413429490342035466'))
+    console.log(await apiModule.getShortLink('test'));
+    console.log(await apiModule.getShortLink('123456789'));
 }
 
 test();
